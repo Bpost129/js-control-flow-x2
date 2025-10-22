5dbb877d8781e0ed75bcf709988326f462230701
Bpost129 <Bpost129@yahoo.com>
2025-10-22 11:25:08 -0400

    test ternaries with functions

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-for (let number = 1; number <= 20; number++) {
-	if (number % 2 === 0) {
-    console.log(`${number} is even`)
-  } else {
-    console.log(`${number} is odd`)
-  }
+// for (let number = 1; number <= 20; number++) {
+// 	if (number % 2 === 0) {
+//     console.log(`${number} is even`)
+//   } else {
+//     console.log(`${number} is odd`)
+//   }
+// }
+
+let score = 95
+
+// let message = score > 100 ? 'You win!' : 'Try again!'
+// console.log(message)
+
+let gameWinner = function(score) {
+  console.log('You win!')
 }
 
-let score = 105
+let gameLoop = function(score) {
+  console.log('Try again!')
+}
 
-let message = score > 100 ? 'You win!' : 'Try again!'
-console.log(message)
+score > 100 ? gameWinner(score) : gameLoop(score)
